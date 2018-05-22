67e92fd48f83a5701d461cb471978f2ea7eec18c
LHT <liht@jsfund.cn>
2018-05-22 16:16:32 +0800

    bla bla

---
+++ utils/src/main/java/cn/imlht/springboot/dubbo/util/Util.java
@@ -0,0 +1,4 @@
+package cn.imlht.springboot.dubbo.util;
+
+public class Util {
+}
---
+++ utils/src/main/java/cn/imlht/springboot/dubbo/util/collect/MapHelper.java
@@ -0,0 +1,18 @@
+package cn.imlht.springboot.dubbo.util.collect;
+
+import java.util.Map;
+import java.util.Objects;
+
+public class MapHelper {
+
+    private MapHelper() {
+    }
+
+    public static boolean isBlank(Map map) {
+        return Objects.isNull(map) || map.isEmpty();
+    }
+
+    public static boolean isNotBlank(Map map) {
+        return !isBlank(map);
+    }
+}
