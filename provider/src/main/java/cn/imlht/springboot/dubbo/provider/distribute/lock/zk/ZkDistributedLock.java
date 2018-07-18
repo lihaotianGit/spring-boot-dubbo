bc578fc6871b0de423adf74a5bfaf3da3a560218
LHT <liht@jsfund.cn>
2018-07-18 14:56:56 +0800

    zk

--- provider/src/main/java/cn/imlht/springboot/dubbo/provider/distribute/lock/zk/ZkDistributedLock.java
+++ provider/src/main/java/cn/imlht/springboot/dubbo/provider/distribute/lock/zk/ZkDistributedLock.java
@@ -1,14 +1,24 @@
 package cn.imlht.springboot.dubbo.provider.distribute.lock.zk;
 
+import cn.imlht.springboot.dubbo.provider.exception.OperationNotSupportedException;
 import org.apache.zookeeper.*;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 
+/**
+ * ZooKeeper 分布式锁
+ * 0. 创建持久性根节点，分布式锁为根节点下的临时有序子节点。
+ * 1. 获取锁：创建子节点并将所有子节点排序，创建最小节点的线程视为持有锁。
+ * 2. 等锁：子节点无变化则持续等锁，到过期时间为止。
+ * 3. 锁续期：不需要。
+ * 4. 解锁：删除目前最小子节点。
+ */
 public class ZkDistributedLock implements Lock, Watcher {
 
     private ZooKeeper zk;
@@ -20,50 +30,64 @@ public class ZkDistributedLock implements Lock, Watcher {
     private String currentLock;
     // 计数器
     private CountDownLatch nodeDeletedLatch;
+    private static final byte[] data = new byte[0];
 
     private int sessionTimeout = 30000;
 
-    /**
-     * 配置分布式锁
-     * @param lockName 竞争资源
-     */
     public ZkDistributedLock(ZooKeeper zk, String lockBase, String lockName) {
         this.zk = zk;
-        this.lockBase = "/" + lockBase;
         this.lockName = lockName;
+        this.lockBase = "/" + lockBase;
         try {
-            // 创建临时有序节点
-            currentLock = zk.create(this.lockBase + "/" + this.lockName, new byte[0], ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
-            System.out.println(Thread.currentThread().getName() + " " + currentLock + " 已经创建");
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+            if (Objects.isNull(zk.exists(this.lockBase, false))) {
+                synchronized (this) {
+                    // 如果根节点不存在，则创建根节点
+                    if (Objects.isNull(zk.exists(this.lockBase, false))) {
+                        zk.create(this.lockBase, data, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+                    }
+                }
+            }
         } catch (KeeperException.NodeExistsException e) {
             System.out.println("NodeExistsException " + Thread.currentThread().getName() + " " + this.lockBase);
         } catch (KeeperException.ConnectionLossException e) {
             System.out.println("ConnectionLossException " + Thread.currentThread().getName() + " " + this.lockBase);
-        } catch (KeeperException e) {
-            e.printStackTrace();
+        } catch (KeeperException | InterruptedException e) {
+            throw new RuntimeException();
         }
+        createSubNode();
     }
 
-    // 节点监视器
+    private void createSubNode() {
+        // 创建临时有序子节点
+        try {
+            currentLock = zk.create(this.lockBase + "/" + this.lockName, data, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
+        } catch (KeeperException | InterruptedException e) {
+            throw new RuntimeException();
+        }
+        System.out.println(Thread.currentThread().getName() + " " + currentLock + " 已经创建");
+    }
+
+    @Override
     public void process(WatchedEvent event) {
         if (this.nodeDeletedLatch != null && event.getType() == Event.EventType.NodeChildrenChanged) {
             nodeDeletedLatch.countDown();
         }
     }
 
+    @Override
     public void lock() {
         try {
             if (!tryLock()) {
-                // 等待锁
-                waitForLock(sessionTimeout);
+                waitForLock(sessionTimeout, TimeUnit.MILLISECONDS);
             }
-        } catch (InterruptedException | KeeperException e) {
-            e.printStackTrace();
+        } catch (InterruptedException e) {
+
+        } catch (KeeperException e) {
+            throw new RuntimeException();
         }
     }
 
+    @Override
     public boolean tryLock() {
         try {
             // 取所有子节点
@@ -75,29 +99,40 @@ public class ZkDistributedLock implements Lock, Watcher {
                 System.out.println(Thread.currentThread().getName() + " 获得了锁" + " " + currentLock);
                 return true;
             }
-        } catch (InterruptedException | KeeperException e) {
-            e.printStackTrace();
+        } catch (InterruptedException e) {
+            unlock();
+        } catch (KeeperException e) {
+            throw new RuntimeException();
         }
         return false;
     }
 
-    public boolean tryLock(long timeout, TimeUnit unit) {
+    @Override
+    public boolean tryLock(long timeout, TimeUnit unit) throws InterruptedException {
         try {
-            return tryLock() || waitForLock(timeout);
-        } catch (Exception e) {
-            e.printStackTrace();
+            return tryLock() || waitForLock(timeout, unit);
+        } catch (InterruptedException e) {
+            throw new InterruptedException();
+        } catch (KeeperException e) {
+            throw new RuntimeException();
         }
-        return false;
     }
 
     // 等待锁
-    private boolean waitForLock(long waitTime) throws KeeperException, InterruptedException {
+    private boolean waitForLock(long waitTime, TimeUnit unit) throws KeeperException, InterruptedException {
         this.nodeDeletedLatch = new CountDownLatch(1);
-        this.nodeDeletedLatch.await(waitTime, TimeUnit.MILLISECONDS);
-        this.nodeDeletedLatch = null;
-        return tryLock(waitTime, TimeUnit.MILLISECONDS);
+        try {
+            this.nodeDeletedLatch.await(waitTime, unit);
+        } catch (InterruptedException e) {
+            unlock();
+            throw new InterruptedException();
+        } finally {
+            this.nodeDeletedLatch = null;
+        }
+        return tryLock(waitTime, unit);
     }
 
+    @Override
     public void unlock() {
         try {
             System.out.println("释放锁 " + currentLock);
@@ -108,10 +143,12 @@ public class ZkDistributedLock implements Lock, Watcher {
         }
     }
 
+    @Override
     public Condition newCondition() {
-        return null;
+        throw new OperationNotSupportedException("Method newCondition() not supported.");
     }
 
+    @Override
     public void lockInterruptibly() throws InterruptedException {
         this.lock();
     }
