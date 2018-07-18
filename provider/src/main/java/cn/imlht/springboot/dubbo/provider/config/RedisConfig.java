ad8384f16e42d1662e0265465d02fdcd95ca0758
LHT <liht@jsfund.cn>
2018-07-18 14:56:47 +0800

    redis

--- provider/src/main/java/cn/imlht/springboot/dubbo/provider/config/RedisConfig.java
+++ provider/src/main/java/cn/imlht/springboot/dubbo/provider/config/RedisConfig.java
@@ -1,18 +1,32 @@
 package cn.imlht.springboot.dubbo.provider.config;
 
 import org.apache.log4j.Logger;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.util.StringUtils;
+import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisSentinelPool;
+import redis.clients.util.Pool;
+
+import javax.annotation.Resource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static java.util.Arrays.asList;
 
 @Configuration
 public class RedisConfig {
 
     private final static Logger logger = Logger.getLogger(RedisConfig.class);
+
+    @Resource
+    private RedisProperties redisProperties;
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate(JedisConnectionFactory conn) {
@@ -34,12 +48,22 @@ public class RedisConfig {
         return redisTemplate;
     }
 
-    @Bean
+    @Bean(destroyMethod = "close")
     public JedisPool jedisPool(JedisConnectionFactory conn) {
         if (StringUtils.isEmpty(conn.getPassword())) {
             return new JedisPool(conn.getPoolConfig(), conn.getHostName(), conn.getPort(), conn.getTimeout());
         } else {
             return new JedisPool(conn.getPoolConfig(), conn.getHostName(), conn.getPort(), conn.getTimeout(), conn.getPassword());
+        }
+    }
+
+    @Bean(destroyMethod = "close")
+    public Pool<Jedis> jedisSentinelPool(JedisConnectionFactory conn) {
+        Set<String> sentinels = new HashSet<>(asList(redisProperties.getSentinel().getNodes().split(",")));
+        if (StringUtils.isEmpty(conn.getPassword())) {
+            return new JedisSentinelPool(redisProperties.getSentinel().getMaster(), sentinels, conn.getPoolConfig(), conn.getTimeout());
+        } else {
+            return new JedisSentinelPool(redisProperties.getSentinel().getMaster(), sentinels, conn.getPoolConfig(), conn.getTimeout(), conn.getPassword());
         }
     }
 
